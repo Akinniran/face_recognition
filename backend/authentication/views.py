ab713c0dd115f1d7abc62d33ea7646928a149366
Akinniran <akinniranoluwatosin22@gmail.com>
2026-07-05 10:54:06 +0100

    Backend architecture setup

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.serializers import FaceEnrollSerializer, FaceScanSerializer, FaceVerifySerializer
+from authentication.serializers import FaceEnrollSerializer, FaceScanSerializer, FaceVerifySerializer
 from face_recognition.face_service.service import FaceRecognitionService, FaceServiceError
 
 
